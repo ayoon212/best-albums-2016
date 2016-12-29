d943bae549bb3af29ca92cf00f5c746170ed20c0
Andy Yoon <ayoon212@gmail.com>
2016-12-29 00:32:25 -0800

    first night styles

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-	$('.tier-contents li').hover(function(event) {
+	var nodes = $('.tier-contents li');
+
+	/*nodes.hover(function(event) {
 		$(event.currentTarget).children('.label').show();
 	}, function(event) {
 		$(event.currentTarget).children('.label').hide();
-	});
+	});*/
+
+	nodes.click(function(event) {
+		var node = $(event.currentTarget).children('.label');
+		if (node.css('display') == 'none') {
+			node.show();
+		} else {
+			node.hide();
+		}
+	})
 });
